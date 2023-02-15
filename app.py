010c76e1377f3018c26b537f2d7c681eed24cde5
Bo4aroff <104056673+Bo4aroff@users.noreply.github.com>
2023-02-15 15:08:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,30 +18,28 @@ st.set_page_config(page_title='History')
 st.title('Вскрываем историю браузера')
 st.subheader('Добавьте ваш файл')
 
-
 uploaded_files = st.file_uploader("ВЫБИРИТЕ СВОЙ ФАЙЛ")
-if uploaded_files:
+# if uploaded_files:
 
     
-        con = sqlite3.connect(uploaded_files)
-        cur = con.cursor()
+#         con = sqlite3.connect(uploaded_files)
+#         cur = con.cursor()
         df1 = pd.read_sql("""SELECT url, title, visit_count, 
-            datetime(last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch', 'localtime') FROM urls""", con)
-
-        df2 = pd.DataFrame(df1)
-        df3 = df2.rename(columns={"datetime(last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch', 'localtime')": "Дата",
-                                          "url": "Адрес",
-                                          "title": "Имя запроса",
-                                          "visit_count": "Посещений страницы"
-                                          })
-        df3['Месяц'] = df3['Дата'].dt.month
-        df3['Год'] = df3['Дата'].dt.year
+            datetime(last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch', 'localtime') FROM urls""", uploaded_files)
+#         df2 = pd.DataFrame(df1)
+#         df3 = df2.rename(columns={"datetime(last_visit_time / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch', 'localtime')": "Дата",
+#                                           "url": "Адрес",
+#                                           "title": "Имя запроса",
+#                                           "visit_count": "Посещений страницы"
+#                                           })
+#         df3['Месяц'] = df3['Дата'].dt.month
+#         df3['Год'] = df3['Дата'].dt.year
         
-if st.checkbox('Сформировать файл для скачивания'):
-    df4 = st.dataframe(df3)
+# if st.checkbox('Сформировать файл для скачивания'):
+#     df4 = st.dataframe(df3)
                 
 
-if st.checkbox('Сформировать ссылку для скачивания'):
+# if st.checkbox('Сформировать ссылку для скачивания'):
 
-    st.subheader('СКАЧАТЬ ФАЙЛ')
-    generate_excel_download_link(df4)
+#     st.subheader('СКАЧАТЬ ФАЙЛ')
+#     generate_excel_download_link(df4)
